6158225f4eee74cad219b3e42bab751942137f9b
dbstj0403 <wcn09088@naver.com>
2023-08-07 22:24:42 +0900

    [홈페이지 배너 슬라이드]

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,13 +2,22 @@ import React from 'react';
 import Introduction from '../components/Introduction';
 import Introduction2 from '../components/Introduction2';
 import Introduction3 from '../components/Introduction3';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay, Navigation, Pagination } from 'swiper';
+import '../componentsCss/Swiper.css';
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
 export default function HomePage () {
-
+    SwiperCore.use([Navigation, Pagination, Autoplay])
     return (
         <div>
-            <Introduction/>
-            <Introduction2/>
-            <Introduction3/>
+            <Swiper pagination={{clickable: true}} loop={true} effect={'fade'} autoplay={{delay: 5000, disableOnInteraction: false}} navigation className='banner'> 
+                <SwiperSlide><Introduction/></SwiperSlide>
+                <SwiperSlide><Introduction2/></SwiperSlide>
+                <SwiperSlide><Introduction3/></SwiperSlide>
+            </Swiper>
         </div>
+        
     );
 }
