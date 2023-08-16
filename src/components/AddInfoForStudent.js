3b6989163f56e014648f539183675a17b3500a2b
dbstj0403 <wcn09088@naver.com>
2023-08-16 18:50:01 +0900

    [import error 고치깅]

--- src/components/AddInfoForStudent.js
+++ src/components/AddInfoForStudent.js
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Postcode from './Postcode';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 export default function AddInfoForStudent () {
     const [name, setName] = useState('');
     const [birthDate, setBirthDate]= useState('');
