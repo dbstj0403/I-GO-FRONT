537baf9e992bfad0650fae69a3acd97cd19b5b94
xognl1124@naver.com <xognl1124@naver.com>
2023-08-16 19:46:41 +0900

    redux install

--- src/pages/LoginCallbackPage.js
+++ src/pages/LoginCallbackPage.js
@@ -1,64 +1,66 @@
 import Spinner from "../components/Spinner";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-export default function LoginCallbackPage () {
-    const CODE = new URL(window.location.href).searchParams.get('code');
-    const [registerState, setRegisterState] = useState(null);
-    const [provider, setProvider] = useState(localStorage.getItem('login-provider'));
-    
-    const selectProvider = () => {
-        if (provider === 'kakao'){
-            sendCode('kakao');
-        }
-        else if (provider === 'google'){
-            sendCode('google');
-        }
-        else {
-            sendCode('github');
-        }
-        console.log('login about:', provider);
-        localStorage.removeItem('login-provider');
-    };
+export default function LoginCallbackPage() {
+  const CODE = new URL(window.location.href).searchParams.get("code");
+  const [registerState, setRegisterState] = useState(null);
+  const [provider, setProvider] = useState(
+    localStorage.getItem("login-provider")
+  );
 
-    const sendCode = async (provider) => {
-        if (registerState === true) {
-        return;
-        }
-        try {
-            const response = await axios.post(
-            `http://localhost:8000/auth/${provider}/token`,
-            { code: CODE }
-        );
-        localStorage.setItem('access-token', response.data.access);
-        localStorage.setItem('refresh-token', response.data.refresh);
-        setRegisterState(response.data.is_register);
-        } catch (error) {
-        console.log("Code sending error!");
+  const selectProvider = () => {
+    if (provider === "kakao") {
+      sendCode("kakao");
+    } else if (provider === "google") {
+      sendCode("google");
+    } else {
+      sendCode("github");
     }
-    };
+    console.log("login about:", provider);
+    localStorage.removeItem("login-provider");
+  };
 
-    const movePage = useNavigate();
-    const moveToPage = () => {
-        if (registerState === true){
-            movePage('/');
-        }
-        else if (registerState === false){
-            movePage('/addInfo');
-        }
-        else{
-            return;
-        }
+  const sendCode = async (provider) => {
+    if (registerState === true) {
+      return;
     }
+    try {
+      const response = await axios.post(
+        `http://localhost:8000/auth/${provider}/token`,
+        { code: CODE }
+      );
+      localStorage.setItem("access-token", response.data.access);
+      localStorage.setItem("refresh-token", response.data.refresh);
+      setRegisterState(response.data.is_register);
+    } catch (error) {
+      console.log("Code sending error!");
+    }
+  };
 
-    useEffect(() => {selectProvider();}, []);
-    useEffect(() => {moveToPage();}, [registerState]);
+  const movePage = useNavigate();
+  const moveToPage = () => {
+    if (registerState === true) {
+      // 로그인 여부 리덕스 스토어 저장
+      movePage("/");
+    } else if (registerState === false) {
+      movePage("/addInfo");
+    } else {
+      return;
+    }
+  };
+
+  useEffect(() => {
+    selectProvider();
+  }, []);
+  useEffect(() => {
+    moveToPage();
+  }, [registerState]);
 
   return (
     <div>
-        <Spinner/>
+      <Spinner />
     </div>
   );
 }
-    
