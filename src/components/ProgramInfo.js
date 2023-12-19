c8e9e8aa2fbfecb86c4194a6ff31d8685baacd36
oonyongs <oonyong99@gmail.com>
2023-12-19 23:35:21 +0900

    오류 여러개 수정,,

--- src/components/ProgramInfo.js
+++ src/components/ProgramInfo.js
@@ -27,271 +27,26 @@ export default function ProgramInfo({ isCarer }) {
 
         }
     }*/
-    const ProgramInfoContainer = styled.div`
-    width: 1008px;
-    margin: auto;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-        width: 720px;
-    }
-    @media screen and (max-width: 768px) {
-        width: 400px;
-    }
-  `;
-    const TitleAndPointContainer = styled.div`
-    width: 1008px;
-    display: flex;
-    height: 54px;
-    align-items: baseline;
-    margin-top: 80px;
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-        width: 720px;
-        height: 40px;
-        margin-top: 60px;
-    }
-    @media screen and (max-width: 768px) {
-        width: 400px;
-        height: 30px;
-        margin-top: 40px;
-    }
-  `;
-    const Title = styled.div`
-    font-size: 24px;
-    line-height: 24px;
-    font-weight: 700;
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-        font-size: 16px;
-    }
-    @media screen and (max-width: 768px) {
-        font-size: 12px;
-    }
-  `;
-    const PointBtn = styled.button`
-    width: 143px;
-    border-radius: 30px;
-    background-color: white;
-    border: 1px solid #cccccc;
-    font-size: 18px;
-    line-height: 18px;
-    align-items: center;
-    display: flex;
-    justify-content: center;
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-        font-size: 16px;
-        line-height: 16px;
-    }
-    @media screen and (max-width: 768px) {
-        font-size: 12px;
-        line-height: 12px;
-    }
-  `;
-    const InfoContainer = styled.div`
-    width: 1010px;
-    height: 286px;
-    border-radius: 10px;
-    border: 1px solid #bfbfbf;
-    margin-top: 10px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-        width: 720px;
-        height: 200px;
-    }
-    @media screen and (max-width: 768px) {
-        width: 400px;
-        height: 140px;
-    }
-  `;
-    const TextContainer = styled.div`
-    width: 910px;
-    height: 38px;
-    display: flex;
-    margin: 10px;
-    font-size: 20px;
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-        width: 600px;
-        height: 30px;
-        font-size: 14px;
-        margin: 5px;
-    }
-    @media screen and (max-width: 768px) {
-        width: 320px;
-        height: 20px;
-        font-size: 10px;
-        margin: 5px;
-    }
-  `;
-    const Text = styled.div`
-    width: 455px;
-    height: 38px;
-    display: flex;
-    align-items: center;
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-        width: 300px;
-        height: 28px;
-    }
-    @media screen and (max-width: 768px) {
-        width: 160px;
-        height: 20px;
-    }
-  `;
-    const TextKind = styled.div`
-    width: 125px;
-    color: #717171;
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-        width: 100px;
-    }
-    @media screen and (max-width: 768px) {
-        width: 60px;
-    }
-  `;
-    const TextAbout = styled.div`
-    width: 330px;
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-        width: 240px;
-    }
-    @media screen and (max-width: 768px) {
-        width: 100px;
-    }
-    `
 
-    const DetailInfoContainer = styled.div`
-    width: 1008px;
-    border-radius: 10px;
-    border: 1px solid #bfbfbf;
-    margin-top: 20px;
-    display: flex;
-    align-items: flex-start;
-    padding: 40px 50px;
-    font-size: 20px;
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-        width: 720px;
-        font-size: 16px;
-        padding: 30px 60px;
-    }
-    @media screen and (max-width: 768px) {
-        width: 400px;
-        font-size: 10px;
-        padding: 20px 40px;
-    }
-  `;
-
-    const DetailInfoText = styled.div`
-    width: 750px;
-    line-height: 24px;
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-        width: 680px;
-        line-height: 20px;
-    }
-    @media screen and (max-width: 768px) {
-        width: 300px;
-        line-height: 16px;
-    }
-    `;
-
-    const BtnContainer = styled.div`
-    width: 378px;
-    height: 67px;
-    display: flex;
-    margin-top: 20px;
-    justify-content: space-between;
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-        width: 300px;
-    }
-    @media screen and (max-width: 768px) {
-        width: 220px;
-    }
-    `
-    const EnrollBtn = styled.button`
-    width: 194px;
-    height: 67px;
-    border-radius: 30px;
-    border: 0;
-    background-color: #FDD888;
-    font-size: 20px;
-    line-height: 20px;
-    font-weight: 700;
-    box-shadow: 5px 5px 5px #CCCCCC;
-    cursor: pointer;
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-        width: 130px;
-        height: 60px;
-        font-size: 16px;
-    }
-    @media screen and (max-width: 768px) {
-        width: 95px;
-        height: 40px;
-        font-size: 12px;
-    }
-    `
-
-    {/*
-    const BackToListBtn = styled.button`
-    width: 150px;
-    height: 67px;
-    border-radius: 30px;
-    border: 0;
-    background-color: #E3E3E3;
-    font-size: 20px;
-    line-height: 20px;
-    font-weight: 700;
-    box-shadow: 5px 5px 5px #CCCCCC;
-    cursor: pointer;
-    `
-    */}
-
-    const BottomBanner = styled.div`
-    width: 1010px;
-    border-radius: 10px;
-    background-color: #F5F5F5;
-    margin-top: 40px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding: 30px 40px;
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-        width: 720px;
-        padding: 20px 40px;
-    }
-    @media screen and (max-width: 768px) {
-        width: 400px;
-        padding: 15px 20px;
-    }
-    
-    `
-    const BottomText = styled.div`
-    font-size: 16px;
-    color: #2C2C2C;
-    margin: 10px;
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-        font-size: 12px;
-        margin: 5px;
-    }
-    @media screen and (max-width: 768px) {
-        font-size: 10px;
-        margin: 5px;
-    }
-    `
     const { id } = useParams();
 
     const [program, setProgram] = useState("");
+    const [address, setAddress] = useState("");
+
     
     const getProgram = async () => {
         try {
             const response = await axios.get(
-                "http://api.igoofficial.com/program/",
+                `http://api.igoofficial.com/program/${id}/`,
                 {
                     // 헤더
                 }
             );
-            console.log(response);
-            console.log(response.data);
-            setProgram(response.data.find((program) => program.id === parseInt(id)))
+            console.log("response.data: ", response.data);
+            setProgram(response.data);
+            setAddress(response.data.address.address);
+            //setProgram(response.data.find((program) => program.id === parseInt(id)))
+            console.log("program: ", program);
         }
         catch {
             alert("Get Program Detail error!")
@@ -338,7 +93,7 @@ export default function ProgramInfo({ isCarer }) {
                     </Text>
                     <Text>
                         <TextKind>주소</TextKind>
-                        <TextAbout>{program.address.address}</TextAbout>
+                        <TextAbout>{address}</TextAbout>
                     </Text>
                 </TextContainer>
                 <TextContainer>
@@ -382,3 +137,253 @@ export default function ProgramInfo({ isCarer }) {
 
     );
 }
+const ProgramInfoContainer = styled.div`
+width: 1008px;
+margin: auto;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+    width: 720px;
+}
+@media screen and (max-width: 768px) {
+    width: 400px;
+}
+`;
+const TitleAndPointContainer = styled.div`
+width: 1008px;
+display: flex;
+height: 54px;
+align-items: baseline;
+margin-top: 80px;
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+    width: 720px;
+    height: 40px;
+    margin-top: 60px;
+}
+@media screen and (max-width: 768px) {
+    width: 400px;
+    height: 30px;
+    margin-top: 40px;
+}
+`;
+const Title = styled.div`
+font-size: 24px;
+line-height: 24px;
+font-weight: 700;
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+    font-size: 16px;
+}
+@media screen and (max-width: 768px) {
+    font-size: 12px;
+}
+`;
+const PointBtn = styled.button`
+width: 143px;
+border-radius: 30px;
+background-color: white;
+border: 1px solid #cccccc;
+font-size: 18px;
+line-height: 18px;
+align-items: center;
+display: flex;
+justify-content: center;
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+    font-size: 16px;
+    line-height: 16px;
+}
+@media screen and (max-width: 768px) {
+    font-size: 12px;
+    line-height: 12px;
+}
+`;
+const InfoContainer = styled.div`
+width: 1010px;
+height: 286px;
+border-radius: 10px;
+border: 1px solid #bfbfbf;
+margin-top: 10px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+    width: 720px;
+    height: 200px;
+}
+@media screen and (max-width: 768px) {
+    width: 400px;
+    height: 140px;
+}
+`;
+const TextContainer = styled.div`
+width: 910px;
+height: 38px;
+display: flex;
+margin: 10px;
+font-size: 20px;
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+    width: 600px;
+    height: 30px;
+    font-size: 14px;
+    margin: 5px;
+}
+@media screen and (max-width: 768px) {
+    width: 320px;
+    height: 20px;
+    font-size: 10px;
+    margin: 5px;
+}
+`;
+const Text = styled.div`
+width: 455px;
+height: 38px;
+display: flex;
+align-items: center;
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+    width: 300px;
+    height: 28px;
+}
+@media screen and (max-width: 768px) {
+    width: 160px;
+    height: 20px;
+}
+`;
+const TextKind = styled.div`
+width: 125px;
+color: #717171;
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+    width: 100px;
+}
+@media screen and (max-width: 768px) {
+    width: 60px;
+}
+`;
+const TextAbout = styled.div`
+width: 330px;
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+    width: 240px;
+}
+@media screen and (max-width: 768px) {
+    width: 100px;
+}
+`
+
+const DetailInfoContainer = styled.div`
+width: 1008px;
+border-radius: 10px;
+border: 1px solid #bfbfbf;
+margin-top: 20px;
+display: flex;
+align-items: flex-start;
+padding: 40px 50px;
+font-size: 20px;
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+    width: 720px;
+    font-size: 16px;
+    padding: 30px 60px;
+}
+@media screen and (max-width: 768px) {
+    width: 400px;
+    font-size: 10px;
+    padding: 20px 40px;
+}
+`;
+
+const DetailInfoText = styled.div`
+width: 750px;
+line-height: 24px;
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+    width: 680px;
+    line-height: 20px;
+}
+@media screen and (max-width: 768px) {
+    width: 300px;
+    line-height: 16px;
+}
+`;
+
+const BtnContainer = styled.div`
+width: 378px;
+height: 67px;
+display: flex;
+margin-top: 20px;
+justify-content: space-between;
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+    width: 300px;
+}
+@media screen and (max-width: 768px) {
+    width: 220px;
+}
+`
+const EnrollBtn = styled.button`
+width: 194px;
+height: 67px;
+border-radius: 30px;
+border: 0;
+background-color: #FDD888;
+font-size: 20px;
+line-height: 20px;
+font-weight: 700;
+box-shadow: 5px 5px 5px #CCCCCC;
+cursor: pointer;
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+    width: 130px;
+    height: 60px;
+    font-size: 16px;
+}
+@media screen and (max-width: 768px) {
+    width: 95px;
+    height: 40px;
+    font-size: 12px;
+}
+`
+
+{/*
+const BackToListBtn = styled.button`
+width: 150px;
+height: 67px;
+border-radius: 30px;
+border: 0;
+background-color: #E3E3E3;
+font-size: 20px;
+line-height: 20px;
+font-weight: 700;
+box-shadow: 5px 5px 5px #CCCCCC;
+cursor: pointer;
+`
+*/}
+
+const BottomBanner = styled.div`
+width: 1010px;
+border-radius: 10px;
+background-color: #F5F5F5;
+margin-top: 40px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+padding: 30px 40px;
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+    width: 720px;
+    padding: 20px 40px;
+}
+@media screen and (max-width: 768px) {
+    width: 400px;
+    padding: 15px 20px;
+}
+
+`
+const BottomText = styled.div`
+font-size: 16px;
+color: #2C2C2C;
+margin: 10px;
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+    font-size: 12px;
+    margin: 5px;
+}
+@media screen and (max-width: 768px) {
+    font-size: 10px;
+    margin: 5px;
+}
+`
--- src/components/ShowPost.js
+++ src/components/ShowPost.js
@@ -5,129 +5,131 @@ import { Link } from "react-router-dom";
 export default function ShowPost({ key, searchList }) {
   console.log("searchList:", searchList);
 
-  const Container = styled.div`
-    width: 1220px;
-    height: 68px;
-    font-size: 14px;
-    line-height: 24px;
-    align-items: center;
-    display: flex;
-    justify-content: center;
-    font-weight: ${(props) => props.bold};
-    border-bottom: 1px solid black;
-    cursor: pointer;
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-      width: 720px;
-      flex-wrap: wrap;
-      margin: auto;
-      height: 90px;
-    }
-    @media screen and (max-width: 768px) {
-      width: 440px;
-      flex-wrap: wrap;
-      margin: auto;
-      height: 60px;
-    }
-  `;
-  const TableTitle1 = styled.div`
-    width: 459px;
-    height: 40px;
-    font-size: 14px;
-    line-height: 24px;
-    align-items: center;
-    display: flex;
-    justify-content: center;
-    font-weight: ${(props) => props.bold};
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-      width: 540px;
-      font-size: 14px;
-      height: 40px;
-      align-items: flex-end;
-    }
-    @media screen and (max-width: 768px) {
-      width: 330px;
-      font-size: 10px;
-      height: 30px;
-      align-items: flex-end;
-    }
-  `;
-  const TableTitle2 = styled.div`
-    width: 160px;
-    height: 40px;
-    font-size: 14px;
-    line-height: 24px;
-    align-items: center;
-    display: flex;
-    justify-content: center;
-    font-weight: ${(props) => props.bold};
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-      width: 180px;
-      font-size: 14px;
-      height: 50px;
-      align-items: flex-start;
-      padding-top: 10px;
-    }
-    @media screen and (max-width: 768px) {
-      width: 110px;
-      font-size: 10px;
-      height: 30px;
-      align-items: flex-start;
-
-    }
-`;
   const IsApplied = styled.div`
-    width: 121px;
-    height: 40px;
-    color: ${searchList.regist_status === "before"
+width: 121px;
+height: 40px;
+color: ${searchList.regist_status === "before"
       ? "black"
       : searchList.regist_status === "now"
-      ? "#2DAB51"
-      : "#D23838"};
-    font-size: 14px;
-    line-height: 24px;
-    align-items: center;
-    display: flex;
-    justify-content: center;
-    font-weight: ${(props) => props.bold};
-    @media screen and (min-width: 769px) and (max-width: 1200px) {
-      width: 180px;
-      font-size: 14px;
-      height: 40px;
-      font-weight: 700;
-      align-items: flex-end;
-    }
-    @media screen and (max-width: 768px) {
-      width: 110px;
-      font-size: 10px;
-      height: 30px;
-      font-weight: 700;
-      align-items: flex-end;
+        ? "#2DAB51"
+        : "#D23838"};
+font-size: 14px;
+line-height: 24px;
+align-items: center;
+display: flex;
+justify-content: center;
+font-weight: ${(props) => props.bold};
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+  width: 180px;
+  font-size: 14px;
+  height: 40px;
+  font-weight: 700;
+  align-items: flex-end;
+}
+@media screen and (max-width: 768px) {
+  width: 110px;
+  font-size: 10px;
+  height: 30px;
+  font-weight: 700;
+  align-items: flex-end;
 
-    }
-  `;
+}
+`
+
   return (
     <Container>
-      <Link to={`/program/${searchList.id}`} style={{textDecoration: "none", display: "flex", flexWrap: "wrap", color: "black"}} >
-      {/*<SmallContent>
+      <Link to={`/program/${searchList.id}`} style={{ textDecoration: "none", display: "flex", flexWrap: "wrap", color: "black" }} >
+        {/*<SmallContent>
 
   </SmallContent>*/}
-      <IsApplied>
-        {searchList.regist_status === "before"
-          ? "모집 전"
-          : searchList.regist_status === "now"
-          ? "모집 중"
-          : "모집 마감"}
-      </IsApplied>
-      <TableTitle1>{searchList.title}</TableTitle1>
-      <TableTitle2>{searchList.activity_category}</TableTitle2>
-      <TableTitle2>{searchList.facility_name}</TableTitle2>
-      <TableTitle2>
-        {searchList.activity_start_at}~{searchList.activity_end_at}
-      </TableTitle2>
-      <TableTitle2 bold="700">
-        {searchList.reward} p
-      </TableTitle2>
+        <IsApplied searchList={searchList}>
+          {searchList.regist_status === "before"
+            ? "모집 전"
+            : searchList.regist_status === "now"
+              ? "모집 중"
+              : "모집 마감"}
+        </IsApplied>
+        <TableTitle1>{searchList.title}</TableTitle1>
+        <TableTitle2>{searchList.activity_category}</TableTitle2>
+        <TableTitle2>{searchList.facility_name}</TableTitle2>
+        <TableTitle2>
+          {searchList.activity_start_at}~{searchList.activity_end_at}
+        </TableTitle2>
+        <TableTitle2 bold="700">
+          {searchList.reward} p
+        </TableTitle2>
       </Link>
     </Container>
   );
 }
+
+const Container = styled.div`
+width: 1220px;
+height: 68px;
+font-size: 14px;
+line-height: 24px;
+align-items: center;
+display: flex;
+justify-content: center;
+font-weight: ${(props) => props.bold};
+border-bottom: 1px solid black;
+cursor: pointer;
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+  width: 720px;
+  flex-wrap: wrap;
+  margin: auto;
+  height: 90px;
+}
+@media screen and (max-width: 768px) {
+  width: 440px;
+  flex-wrap: wrap;
+  margin: auto;
+  height: 60px;
+}
+`;
+const TableTitle1 = styled.div`
+width: 459px;
+height: 40px;
+font-size: 14px;
+line-height: 24px;
+align-items: center;
+display: flex;
+justify-content: center;
+font-weight: ${(props) => props.bold};
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+  width: 540px;
+  font-size: 14px;
+  height: 40px;
+  align-items: flex-end;
+}
+@media screen and (max-width: 768px) {
+  width: 330px;
+  font-size: 10px;
+  height: 30px;
+  align-items: flex-end;
+}
+`;
+const TableTitle2 = styled.div`
+width: 160px;
+height: 40px;
+font-size: 14px;
+line-height: 24px;
+align-items: center;
+display: flex;
+justify-content: center;
+font-weight: ${(props) => props.bold};
+@media screen and (min-width: 769px) and (max-width: 1200px) {
+  width: 180px;
+  font-size: 14px;
+  height: 50px;
+  align-items: flex-start;
+  padding-top: 10px;
+}
+@media screen and (max-width: 768px) {
+  width: 110px;
+  font-size: 10px;
+  height: 30px;
+  align-items: flex-start;
+
+}
+`;
